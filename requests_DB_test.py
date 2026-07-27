90a3206f50e143302737572ab397086bd931b96e
Gogik <gog.ortey@yandex.ru>
2026-07-27 21:47:12 +0500

    Начинаю собирать запросы к БД

---
+++ requests_DB_test.py
@@ -0,0 +1,8 @@
+"""
+
+curl -X 'GET' \
+  'https://skynode.nocodb.api.gogortey.ru/api/v3/data/p6iywpukq1yiryf/meooj41uwpyrx9t/records' \
+  -H 'accept: application/json' \
+  -H 'xc-token: nc_pat_0AJ...Mhd' (лежит в .env в переменной NOCODB_SKYNODE_API_KEY)
+
+"""
